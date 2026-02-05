eff451b924ba15cb7bbdd3803e3ab568e6a4ff3d
Côme Périn <come.perin@bordeaux-inp.fr>
2026-02-06 00:11:06 +0100

    file change

--- deicisonTree.cpp
+++
@@ -1,9 +0,0 @@
-#include "decisionTree.h"
-
-DecisionTree::DecisionTree(DataSet* dataSet, int maxDepth, int minSamplesSplit) 
-    : dataSet(dataSet), maxDepth(maxDepth), minSamplesSplit(minSamplesSplit), root(std::make_unique<TreeNode>(dataSet)) {
-}
-
-TreeNode::TreeNode(DataSet* dataSet, int featureIndex, float featureValue) 
-    : dataSet(dataSet), featureIndex(featureIndex), featureValue(featureValue) {
-}
